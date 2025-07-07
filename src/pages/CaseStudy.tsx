5d7be95b038fd5557dc68afa11eed29fd755ae1f
Udit Sangwan <sangwanhq@192.168.1.2>
2025-07-07 10:14:56 +0530

    Updated homepage content and layout

--- src/pages/CaseStudy.tsx
+++ src/pages/CaseStudy.tsx
@@ -18,7 +18,7 @@ const caseStudies = [
     id: 'zepto',
     name: 'Zepto',
     description: '10-minute grocery. Dark stores. The scale war.',
-    logo: '/logos/zepto.png',
+    logo: '/logos/Zepto.png',
     url: 'https://sangwanhq.substack.com/p/zepto-isnt-a-grocery-app-its-a-wager',
     tag: 'Latest',
   },
